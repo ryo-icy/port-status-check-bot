cb29f35b966db675b379876cd2d5c569c747e486
Ryo-icy <ryo.win.id@gmail.com>
2024-03-03 15:47:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# Port Status Check Bot(Discord)
+ポート監視(TCP/UDP)の結果をDiscordに投稿するBot(Webhook)
+
+## イメージ
+![](./doc/img/post_image.png)
+
+## Dockerコンテナで実行する。
+1. DiscordのWebhookを取得する。
+
+> [!NOTE]
+> 本BOTはデフォルトで結果を連投します。1つのメッセージを永遠に更新し続けたい場合はメッセージIDを取得する必要があります。
+
+2. `git clone https://github.com/ryo-icy/port-status-check-bot.git`
+
+3. `config.yaml`を準備
+    ```
+    portcheck:
+    - name: <Name>
+        host: <IP or FQDN>
+        port: <Port>
+        protocol: <TCP or UDP>
+    ```
+    ### 例
+    ```
+    portcheck:
+    - name: Test1
+        host: 192.168.1.222
+        port: 30100
+        protocol: TCP
+    - name: Test2
+        host: 192.168.1.222
+        port: 30100
+        protocol: UDP
+    ```
+
+4. `docker-compose.yaml`の項目を自分の環境に合わせる。
+    ```
+    environment:
+      SCB_WEBHOOK: ${WEBHOOK}
+      SCB_MESSAGE_ID: ${MESSAGE_ID}
+      SCB_INTERVAL: 30
+    volumes:
+      - ./app/script/config.yaml:/script/config.yaml:ro
+    ```
+
+    ### パラメータ
+    | パラメータ | 必須 | 説明 |
+    | --- | --- | --- |
+    | SCB_WEBHOOK | True | DiscordのWebhookURLを指定 |
+    | SCB_MESSAGE_ID | False | メッセージを連投ではなく、1つのメッセージを更新し続ける際に指定<br>指定するIDは使用するWebhookが投稿したものでなければならない |
+    | SCB_INTERVAL | True | ステータスをチェックするインターバルを指定<br>1以上の整数 |
+
+5. `docker compose up -d`
+
+
+## Python実行
+1. DiscordのWebhookを取得する。
+
+2. `git clone https://github.com/ryo-icy/port-status-check-bot.git`
+
+3. `cd ./app/script`
+
+4. `config.yaml`を準備
+    ```
+    portcheck:
+    - name: <Name>
+        host: <IP or FQDN>
+        port: <Port>
+        protocol: <TCP or UDP>
+    ```
+
+5. `local_setting.py`を作成
+    ```
+    SCB_MESSAGE_ID = ""
+    SCB_WEBHOOK = ""
+    SCB_INTERVAL = 30
+    ```
+
+6. `pipenv install -r requirements.txt`
+
+7. `pipenv run python3 ./discordbot.py`
+
+> [!NOTE]
+> 環境変数でも可能
---
+++ app/script/discordbot.py
@@ -0,0 +1,103 @@
+import datetime
+import json
+import os
+import socket
+import time
+
+import requests
+import yaml
+
+debug_run = False
+try:
+    from local_setting import *
+    debug_run = True
+except:
+    SCB_WEBHOOK = os.environ['SCB_WEBHOOK']
+    SCB_MESSAGE_ID = os.environ['SCB_MESSAGE_ID']
+    SCB_INTERVAL = os.environ['SCB_INTERVAL']
+
+webhook = SCB_WEBHOOK
+messageid = SCB_MESSAGE_ID
+interval = int(SCB_INTERVAL)*60 # 分に直す
+
+# ステータス絵文字
+running = ':white_check_mark:'
+stop = ':x:'
+
+class PortCheck:
+    def __init__(self, config_path='/script/config.yaml'):
+        with open(config_path, encoding='utf-8') as file:
+            config = yaml.safe_load(file)
+        self.config_yaml = config
+    
+    def _portcheck(self, config):
+        queue = []
+        if config['protocol'] == 'TCP':
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((socket.gethostbyname(config['host']), config['port']))
+        elif config['protocol'] == 'UDP':
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.sendto(b'test', (socket.gethostbyname(config['host']), config['port']))
+            sock.settimeout(1)
+            try:
+                sock.recv(1024)
+                result = 0
+            except:
+                result = 1
+        
+        sock.close()
+        if result == 0:
+            return running
+        else:
+            return stop
+
+    def _get_now_time(self):
+        dt_now = datetime.datetime.now()
+        dt_now = str(dt_now)[:16]
+        dt_now = dt_now.replace('-', '/')
+
+        return dt_now
+
+    def _post_discord(self):
+        header = {'Content-Type': 'application/json'}
+        mes = {
+            "embeds": [
+                {
+                    "title": 'Minecraft Server Status Check',
+                    "color": 65280,
+                    "footer": {
+                        "text": f"last check: {self._get_now_time()}"
+                    }
+                }
+            ]
+        }
+        body = [
+                {
+                    'name': '{}'.format(config['name']),
+                    'value': '{}'.format(self._portcheck(config=config)),
+                    'inline': True
+                }for config in self.config_yaml['portcheck']
+            ]
+        mes['embeds'][0]['fields'] = body
+
+        if len(messageid) == 0:
+            requests.post(webhook, json.dumps(mes), headers= header)
+        else:
+            requests.patch(webhook + '/messages/' + messageid, json.dumps(mes), headers=header)
+
+    def run(self):
+        while True:
+            self._post_discord()
+            time.sleep(interval)
+
+
+
+if __name__ == '__main__':
+    # デバッグ
+    if debug_run:
+        # Windowsを想定
+        App = PortCheck(r'.\config.yaml')
+    else:
+        App = PortCheck()
+
+    App.run()
